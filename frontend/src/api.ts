bb57a410c6e11113af27af3abafe8634c0a62088
LionelGoffaux <lionelg@outlook.be>
2022-09-25 14:09:35 +0200

    prod fix

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "https://api.infocalendar.lionel-goffaux.be/api",
+  baseURL: "https://api." + location.hostname + "/api",
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
