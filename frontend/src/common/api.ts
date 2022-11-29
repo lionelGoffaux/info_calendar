cee720f40bcdb760ec5658afb958a24982cf5918
LionelGoffaux <lionelg@outlook.be>
2022-11-29 11:06:51 +0100

    fix[frontend]: fix api endpoint

--- frontend/src/common/api.ts
+++ frontend/src/common/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: import.meta.env.VITE_API_ENDPOINT,
+  baseURL: import.meta.env.VITE_API_ENDPOINT + "/api",
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
